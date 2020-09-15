163e13f471a83611d453255e09161980be6d0273
snehal-mane <snehal6mane@gmail.com>
2020-09-15 18:41:07 +0530

    added date input

--- Company/add-new-tour/script.js
+++ Company/add-new-tour/script.js
@@ -1,1 +1,6 @@
-$('textarea').autoResize();
+var today = new Date();
+var lastDate = new Date(today.getFullYear(), today.getMonth(0), 1);
+$(function () {
+  $("#datepicker").datepicker({ minDate: 0, maxDate: "+1M" });
+  $("#datepicker").datepicker("option", "dateFormat", "DD, d MM, yy");
+});
