e5ce6ff60d92229aa0e6a7b5831ee4bd3bad5c42
snehal-mane <snehal6mane@gmail.com>
2020-11-05 21:18:49 +0530

    added 'view tours' and 'scheduled tours' page

--- Company/scheduled-tours/scheduled-tours.php
+++ Company/scheduled-tours/scheduled-tours.php
@@ -1,6 +1,6 @@
 <?php
 // TODO change company_id
-$url = 'http://localhost/IVMS-API/API/tour/getCompanyTours.php?company_id=1';
+$url = 'http://localhost/IVMS-API/API/bookedTour/getCompanyBookedTours.php?company_id=1';
 $json_data = file_get_contents($url);
 $tourArray = json_decode($json_data, true);
 ?>
@@ -11,7 +11,7 @@ $tourArray = json_decode($json_data, true);
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Your Tours</title>
+    <title>Scheduled Tours</title>
     <link rel="stylesheet" href="style.css" />
 </head>
 
@@ -20,34 +20,32 @@ $tourArray = json_decode($json_data, true);
     <header class="header">
         <h1>Industrial Visit Management System</h1>
         <div class="header-right">
-            <h5>Your Tours</h5>
+            <h5>Your Scheduled Tours</h5>
         </div>
     </header>
 
-    <!-- sidebar -->
     <div class="sidebar">
         <img src="../images/logo.png" alt="" width="180" />
         <div class="sidebar-links">
             <a href="../company-dashboard/company-dashboard.php">Dashboard</a>
             <a href="../add-new-tour/add-new-tour.php">Add New Tour</a>
-            <a href="" id="active">View Your Tours</a>
-            <a href="../scheduled-tours/scheduled-tours.html">View Scheduled Tours</a>
+            <a href="../your-tours/your-tours.html">View Your Tours</a>
+            <a href="" id="active">View Scheduled Tours</a>
             <a href="">View Past Tours</a>
         </div>
     </div>
 
-    <!-- main content -->
     <main class="content">
-        <!-- content header -->
+        <!-- content heading -->
         <header class="content-header">
-            <h2 id="main-heading">Your Tours</h2>
+            <h2 id="main-heading">Your Scheduled Tours</h2>
             <div class="content-header-icons">
                 <a href=""><img src="../images/user.svg" alt="" width="35" /></a>
                 <a href=""><img src="../images/logout.svg" alt="" width="32" /></a>
             </div>
         </header>
 
-        <!-- tour cards -->
+        <!-- scheduled tours -->
         <div class="tour-container">
             <?php
             if (isset($tourArray['data'][0]['message'])) {
@@ -57,39 +55,14 @@ $tourArray = json_decode($json_data, true);
             } else {
                 foreach ($tourArray['data'] as $key => $item) {
                     echo '<section class="tour-card">
-          <div class="tour-options">
-            <a href=""
-              ><img
-                src="../images/edit-regular.svg"
-                alt="edit tour"
-                class="option"
-            /></a>
-            <a href=""
-              ><img
-                src="../images/trash-solid.svg"
-                alt="delete tour"
-                class="option"
-            /></a>
-          </div>
           <div class="tour-heading">
             <h2 class="tour-title">' . $item['name'] . '</h2>
-            <h3 class="tour-branch">' . $item['branch'] . '</h3>
-            <h3 class="tour-date">' . $item['available_days'] . '</h3>
-            <h3>' . $item['rate'] . '</h3>
+            <h3 class="tour-college">' . $item['college'] . '</h3>
+            <h3 class="tour-date">' . $item['date'] . '</h3>
           </div>
           <div class="tour-details">
-            <h3 class="tour-place">' . $item['place'] . '</h3>
-            <h4 class="tour-people">Capacity: ' . $item['number_people'] . '</h4>
-            <h4 class="tour-times">Created at: ' . substr($item['created_at'], 0, 10) . '</h4>
-            <h4 class="tour-times">Edited at: ' . substr($item['edited_at'], 0, 10) . '</h4>
-          </div>
-          <div class="description">
-            <p class="tour-description">
-              ' . $item['description'] . '
-            </p>
-            <div class="rating">
-              <h3 class="tour-rating">Average Rating: ' . $item['avg_rating'] . '</h3>
-            </div>
+            <h3 class="tour-booked">Booked at: ' . $item['booked_at'] . '</h3>
+            <h4 class="tour-people">Number of Attendees: ' . $item['number_people'] . '</h4>
           </div>
         </section>';
                 }
