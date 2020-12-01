4cfab9b949f914295496afdf8b816865cf9d730f
savani dalvi <savani.dalvi99@gmail.com>
2020-12-01 23:21:53 +0530

    coordinator files added

---
+++ Coordinator/Edit-profile/edit_profile.php
@@ -0,0 +1,92 @@
+<?php
+// TODO change company id
+$companyID = 1;
+$url = "http://localhost/IVMS-API/API/company/getCompanyInfo.php?company_id=1";
+$json_data = file_get_contents($url);
+$company = json_decode($json_data, true);
+$company = $company['data'][0];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Profile</title>
+    <script>
+        const companyID = <?php echo $companyID ?>
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.0/axios.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="edit_profile.js"></script>
+    <link rel="stylesheet" href="edit_profile.css" />
+</head>
+
+<body>
+    <div class="header">
+        <h1>Industrial Visit Management System</h1>
+        <div class="header-right">
+            <h5>Add New Tour</h5>
+        </div>
+    </div>
+
+    <div class="sidebar">
+        <img src="../images/logo.png" alt="" width="180" />
+        <div class="sidebar-links">
+            <a href="../Coordinator-dashboard/coordinator-dashboard.php">Dashboard</a>
+            <a href="../your-tours/your-tours.php">View Your Tours</a>
+            <a href="../scheduled-tours/scheduled-tours.php">View Scheduled Tours</a>
+            <a href="">View Past Tours</a>
+        </div>
+    </div>
+
+    <main class="content">
+        <h3 class="form-heading">Edit Profile</h3>
+        <form name="profile" onSubmit="return formValidation()" method="post">
+            <div class="form-element">
+                <label for="company" class="form-label">Coordinator ID</label>
+                <input type="text" name="name" class="form-input" required value="<?= $company['name'] ?>" />
+                <p class="error" id="name-error">Please enter a valid name.</p>
+            </div>
+
+            <div class="form-element">
+                <label for="phone_number" class="form-label">College</label>
+                <input type="text" name="phone_number" class="form-input" required value="<?= $company['phone_number'] ?>" />
+                <p class="error" id="phone_number-error">Please enter a valid phone number.</p>
+            </div>
+
+            <div class="form-element">
+                <label for="description" class="form-label">Coordinator email</label>
+                <input type="text" name="description" class="form-input" required value="<?= $company['description'] ?>" />
+                <p class="error" id="description-error">Please enter a valid description.</p>
+            </div>
+
+            <button type="submit" class="edit-profile-btn">edit profile</button>
+            <button type="button" id="change-password" class="edit-profile-btn" onclick="password()">change password</button>
+        </form>
+    </main>
+
+    <div class="footer">
+        <div class="socials">
+            <div class="site twitter">
+                <img src="../images/twitter-square-brands.svg" alt="" width="15px" />
+                <a href="">Twitter</a>
+            </div>
+            <div class="site facebook">
+                <img src="../images/facebook-square-brands.svg" alt="" width="15px" />
+                <a href="">Facebook</a>
+            </div>
+            <div class="site instagram">
+                <img src="../images/instagram-brands.svg" alt="" width="15px" />
+                <a href="">Instagram</a>
+            </div>
+        </div>
+        <div class="contact">
+            <h4>Email Address: contact@ivms.com</h4>
+            <h4>Contact Number: 9876543219</h4>
+        </div>
+    </div>
+</body>
+
+</html>
