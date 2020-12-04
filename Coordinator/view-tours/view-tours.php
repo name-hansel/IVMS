918e0ddc6552404b9aba68101773be5c6c3c33d3
name-hansel <snehal6mane@gmail.com>
2020-12-04 22:13:57 +0530

    added coordinator view-tours css

--- Coordinator/view-tours/view-tours.php
+++ Coordinator/view-tours/view-tours.php
@@ -14,7 +14,7 @@ $tour_array = json_decode($json_data, true);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Available Tours</title>
-    <link rel="stylesheet" href="../coordinator-dashboard/style.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
